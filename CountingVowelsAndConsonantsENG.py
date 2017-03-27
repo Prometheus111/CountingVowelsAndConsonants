757fb7dbe908182936053dedd01d0965c8e91c9b
Alex Tretyakov <tretyakov-a.n@yandex.ru>
2017-03-27 18:31:44 +0300

    Update CountingVowelsAndConsonantsENG.py

--- CountingVowelsAndConsonantsENG.py
+++ CountingVowelsAndConsonantsENG.py
@@ -5,9 +5,9 @@
 
 #request to enter and translate the entered letters into lowercase
 i = input('Enter the text: \n').lower()
-#vowels & consonants for ENG
-g = ['a','e','i','o','u','y']
-s = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z']
+#arrays for ENG
+vowels = ['a','e','i','o','u','y']
+consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z']
 #output the results
 print('Number of vowels: ', len([1 for x in list(i) if x in g]))
 print('Number of consonants: ', len([1 for x in list(i) if x in s]))
