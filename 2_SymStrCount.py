046b80ea62f01bbcaaf473b3ba05167aa9c144e3
Alex Tretyakov <tretyakov-a.n@yandex.ru>
2017-03-31 18:43:35 +0300

    Update and rename symstrcount.py to 2_SymStrCount.py

---
+++ 2_SymStrCount.py
@@ -0,0 +1,19 @@
+# 2_SymStrCount.py
+# by Prometheus111
+# for a given string, calculate the number of occurrences of a character in it
+# that is read from the keyboard
+s = 'Here is your own text!'            #we have the string s
+print(s)                                #output the string s
+a = input('Input the character: ')
+n = 0
+for i in s:                             #checking each character in a string
+    if i == a:                          #if the character is in the string
+        n += 1                          #count it
+    else:                               #if the character isn't in the string
+        pass                            #do nothing
+if n >= 1:                              #if found
+    print('The character "%s" occurs in the string %d times' % (a, n))
+else:                                   #if not found
+    print('The character "%s" in the string does not occur' %a)
+# Enjoy learning new things!
+# https://github.com/Prometheus111 
